effddaf8e80ae4bf4fcfd4e7bf26bbac8a724006
root <root@nyus.joshuawise.com>
2009-05-21 22:21:04 -0400

    Move superblock logic to main function

--- e3view.c
+++ e3view.c
@@ -138,52 +138,54 @@ void show_block_group_desc_table(struct ext2_super_block *sb)
 	}
 }
 
-void show_superblock(sector_t s)
+void show_superblock(struct ext2_super_block *sb)
 {
-	struct ext2_super_block sb;
-	
-	if (disk_read_sector(s, (uint8_t*)&sb) < 0)
-	{
-		perror("read_sector");
-		return;
-	}
-	
-	printf("Superblock from sector %d\n", s);
-	printf("\tMagic         : 0x%04X (%s)\n", sb.s_magic, (sb.s_magic == 0xEF53) ? "correct" : "INCORRECT");
-	printf("\tInodes        : %d\n", sb.s_inodes_count);
-	printf("\tBlocks        : %d\n", sb.s_blocks_count);
-	printf("\tBlock size    : %d\n", 1024 << sb.s_log_block_size);
-	printf("\t   on-disk    : %d\n", sb.s_log_block_size);
-	printf("\tFragment size : %d\n", (sb.s_log_frag_size > 0) ? (1024 << sb.s_log_block_size) : (1024 >> -sb.s_log_block_size));
-	printf("\t   on-disk    : %d\n", sb.s_log_frag_size);
-	printf("\tJournal inode : %d\n", sb.s_journal_inum);
-	printf("\tThis SB from  : %d\n", sb.s_block_group_nr);
+	printf("Superblock from block group %d\n", sb->s_block_group_nr);
+	printf("\tMagic         : 0x%04X (%s)\n", sb->s_magic, (sb->s_magic == 0xEF53) ? "correct" : "INCORRECT");
+	printf("\tInodes        : %d\n", sb->s_inodes_count);
+	printf("\tBlocks        : %d\n", sb->s_blocks_count);
+	printf("\tBlock size    : %d\n", 1024 << sb->s_log_block_size);
+	printf("\t   on-disk    : %d\n", sb->s_log_block_size);
+	printf("\tFragment size : %d\n", (sb->s_log_frag_size > 0) ? (1024 << sb->s_log_block_size) : (1024 >> -sb->s_log_block_size));
+	printf("\t   on-disk    : %d\n", sb->s_log_frag_size);
+	printf("\tJournal inode : %d\n", sb->s_journal_inum);
 	printf("\n");
 	printf("Group information:\n");
-	printf("\tFirst data block : %d\n", sb.s_first_data_block);
-	printf("\tBlocks per       : %d\n", sb.s_blocks_per_group);
-	printf("\tFragments per    : %d\n", sb.s_frags_per_group);
-	printf("\tInodes per       : %d\n", sb.s_inodes_per_group);
-	if (sb.s_feature_ro_compat & EXT2_FEATURE_RO_COMPAT_SPARSE_SUPER)
+	printf("\tFirst data block : %d\n", sb->s_first_data_block);
+	printf("\tBlocks per       : %d\n", sb->s_blocks_per_group);
+	printf("\tFragments per    : %d\n", sb->s_frags_per_group);
+	printf("\tInodes per       : %d\n", sb->s_inodes_per_group);
+	if (sb->s_feature_ro_compat & EXT2_FEATURE_RO_COMPAT_SPARSE_SUPER)
 		printf("\tSuperblocks are SPARSE! Available at block groups 0, 1, powers of 3, powers of 5, powers of 7.\n");
 	printf("\n");
-	if (sb.s_log_block_size < 1)
+	if (sb->s_log_block_size < 1)
 	{
 		printf("1k blocks are not supported! (Potentially bad superblock?)\n");
 		return;
 	}
 	
 	printf("OK, so expect:\n");
-	printf("\tBlock groups         : %d\n", sb.s_blocks_count / sb.s_blocks_per_group);
-	printf("\t   (Blocks left over?) : %d\n", sb.s_blocks_count % sb.s_blocks_per_group);
-	printf("\n");
-	
-	show_block_group_desc_table(&sb);
+	printf("\tBlock groups         : %d\n", sb->s_blocks_count / sb->s_blocks_per_group);
+	printf("\t   (Blocks left over?) : %d\n", sb->s_blocks_count % sb->s_blocks_per_group);
 }
 
 int main()
 {
-	printf("Trying to read superblock...\n");
-	show_superblock(2);
+	struct ext2_super_block sb;
+	
+	printf("Reading superblock from sector 2...\n");
+	if (disk_read_sector(2, (uint8_t*)&sb) < 0)
+	{
+		perror("read_sector");
+		return;
+	}
+	
+	printf("Dumping superblock:\n");
+	show_superblock(&sb);
+	printf("\n");
+	
+	printf("Dumping block group descriptor table:\n");
+	show_block_group_desc_table(&sb);
+	printf("\n");
 	return 0;
 }
