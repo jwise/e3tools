9c5bcb32349e7e81edcd07cdf1e0235a28c0a0c9
Joshua Wise <joshua@rebirth.joshuawise.com>
2009-05-25 00:53:01 -0400

    Split out code to check vs. show inode tables, and have those be command line options

--- e3view.c
+++ e3view.c
@@ -38,7 +38,7 @@ void inode_table_show(struct ext2_super_block *sb, int bg)
 	int bogus = 0;
 	
 	printf("Inode table from block group %d\n", bg);
-	//printf("Starts at block %d, should contain %d inodes in %d blocks\n", curblock, inodes, blocks);
+	printf("Starts at block %d, should contain %d inodes in %d blocks\n", curblock, inodes, blocks);
 	for (b = 0; b < blocks; b++)
 	{
 		int i;
@@ -51,8 +51,16 @@ void inode_table_show(struct ext2_super_block *sb, int bg)
 		{
 			struct ext2_inode *inode = (struct ext2_inode *)(block + sb->s_inode_size * i);
 			char *ftype;
+			int ino = i + b * inodes_per_block + bg * inodes + 1;
 			
-		//	printf("\tInode %d\n", i + b * inodes_per_block + bg * inodes + 1);
+			printf("\tInode %d%s\n", ino,
+				(ino == 1) ? " (badblocks)" :
+				(ino == 2) ? " (root directory)" :
+				(ino == 5) ? " (bootloader)" :
+				(ino == 6) ? " (undelete)" :
+				(ino == 7) ? " (extra group descriptors)" :
+				(ino == 8) ? " (journal)" :
+				"");
 			switch (inode->i_mode & 0xF000)
 			{
 			case 0x0000: ftype = "empty"; break;
@@ -65,18 +73,74 @@ void inode_table_show(struct ext2_super_block *sb, int bg)
 			case 0xC000: ftype = "socket"; break;
 			default: ftype = "bogus file type!"; break;
 			}
-		//	printf("\t\tMode   : %04X (%s, %o)\n", inode->i_mode, ftype, inode->i_mode & 07777);
-		//	printf("\t\tLinks  : %d%s\n", inode->i_links_count,
-		//		(inode->i_links_count > 1024) ? " (looks bogus!)" : " (looks OK)");
-		//	printf("\t\tBlocks : %d\n", inode->i_blocks);
-			if (inode->i_links_count > 4096)
+			printf("\t\tMode   : %04X (%s, %o)%s\n", inode->i_mode, ftype, inode->i_mode & 07777,
+				(((inode->i_mode) & 0xF000 == 0) && inode->i_mode) ? " (looks bogus!)" : "");
+			printf("\t\tLinks  : %d%s\n", inode->i_links_count,
+				(inode->i_links_count > 1024) ? " (looks bogus!)" : "");
+			printf("\t\tBlocks : %d\n", inode->i_blocks);
+			printf("\t\tFlags  : ");
+			if (inode->i_flags & 0x00000001)
+				printf("secrm ");
+			if (inode->i_flags & 0x00000002)
+				printf("unrm ");
+			if (inode->i_flags & 0x00000004)
+				printf("compr ");
+			if (inode->i_flags & 0x00000008)
+				printf("sync ");
+			if (inode->i_flags & 0x00000010)
+				printf("immutable ");
+			if (inode->i_flags & 0x00000020)
+				printf("append ");
+			if (inode->i_flags & 0x00000040)
+				printf("nodump ");
+			if (inode->i_flags & 0x00000080)
+				printf("noatime ");
+			if (inode->i_flags & 0x00010000)
+				printf("btreedir/hashdir ");
+			if (inode->i_flags & 0x00020000)
+				printf("afs ");
+			if (inode->i_flags & 0x00040000)
+				printf("journal ");
+			if (inode->i_flags & 0xFFF80000)
+				printf("type A? ");
+			printf("\n");
+		}
+		curblock++;
+	}
+}
+
+void inode_table_check(struct ext2_super_block *sb, int bg)
+{
+	int curblock = block_group_inode_table_block(sb, bg);
+	int inodes = sb->s_inodes_per_group;
+	int inodes_per_block = SB_BLOCK_SIZE(sb) / sb->s_inode_size;
+	int blocks = inodes * sb->s_inode_size / SB_BLOCK_SIZE(sb);
+	uint8_t *block = alloca(SB_BLOCK_SIZE(sb));
+	int b;
+	int ok = 0;
+	int bogus = 0;
+	
+	for (b = 0; b < blocks; b++)
+	{
+		int i;
+		if (disk_read_block(sb, curblock, (uint8_t *)block) < 0)
+		{
+			perror("disk_read_block");
+			return;
+		}
+		for (i = 0; i < inodes_per_block; i++)
+		{
+			struct ext2_inode *inode = (struct ext2_inode *)(block + sb->s_inode_size * i);
+
+			if ((inode->i_links_count > 4096) || 
+			    (((inode->i_mode) & 0xF000 == 0) && inode->i_mode))
 				bogus++;
 			else
 				ok++;
 		}
 		curblock++;
 	}
-	printf("%d OK inodes, %d bogus inodes\n", ok, bogus);
+	printf("Inode table from block group %d: %d OK inodes, %d bogus inodes\n", bg, ok, bogus);
 }
 
 int main(int argc, char **argv)
@@ -87,6 +151,8 @@ int main(int argc, char **argv)
 	int print_sb = 0;
 	int print_bgd = 0;
 	int repair_bgd = 0;
+	int check_itable = 0;
+	int show_itable = -1;
 	int i;
 	
 	printf("e3view " VERSION "\n"
@@ -94,7 +160,7 @@ int main(int argc, char **argv)
 	       "Using it without care might make your drive into one of them. Create an\n"
 	       "LVM snapshot before using this program.\n\n");
 	
-	while ((opt = getopt(argc, argv, "n:sdD")) != -1)
+	while ((opt = getopt(argc, argv, "n:sdDTt:")) != -1)
 	{
 		switch (opt)
 		{
@@ -110,12 +176,20 @@ int main(int argc, char **argv)
 		case 'D':
 			repair_bgd = 1;
 			break;
+		case 'T':
+			check_itable = 1;
+			break;
+		case 't':
+			show_itable = strtoll(optarg, NULL, 0);
+			break;
 		default:
-			printf("Usage: %s [-n sector_of_alt_superblock] [-s] [-d] [-D]\n", argv[0]);
+			printf("Usage: %s [-n sector_of_alt_superblock] [-s] [-d] [-D] [-i block_group] [-I]\n", argv[0]);
 			printf("-n causes the superblock to be read from an alternate location\n");
 			printf("-s enables printing of Superblock information\n");
 			printf("-d enables printing of block group Descriptor information\n");
 			printf("-D enables repair of block group Descriptor information\n");
+			printf("-T enables check/salvage of inode table information\n");
+			printf("-t displays the contents of a block group's inode table\n");
 			exit(1);
 		}
 	}
@@ -148,8 +222,12 @@ int main(int argc, char **argv)
 		printf("\n");
 	}
 	
-	for (i = 0; i < SB_GROUPS(&sb); i++)
-		inode_table_show(&sb, i);
+	if (show_itable >= 0)
+		inode_table_show(&sb, show_itable);
+	
+	if (check_itable)
+		for (i = 0; i < SB_GROUPS(&sb); i++)
+			inode_table_check(&sb, i);
 	
 	return 0;
 }
